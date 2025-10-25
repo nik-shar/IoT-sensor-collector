d92a4e3ffe882aad248f83ac392dc4e2b23f7cae
nik-shar <gopalpandey386@gmail.com>
2025-10-25 19:12:16 +0530

    CORE handling

--- app/main.py
+++ app/main.py
@@ -21,9 +21,15 @@ app = FastAPI(title="IoT Sensor Collector API", lifespan=lifespan)
 led_state = {"led": "off"}
 
 # CORS
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://iot-sensor-collector.onrender.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://iot-sensor-collector.onrender.com",],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
